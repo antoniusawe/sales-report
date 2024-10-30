b2e3030b3c7d6db0c1b10e27afcff2daa54479e4
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-10-30 09:08:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,17 @@ else:
         url = "https://raw.githubusercontent.com/antoniusawe/student_database/main/student_database_200hr.xlsx"
         data_200hr = pd.read_excel(url)
 
+        # Calculate Total Booking by counting non-null entries in "Name of student"
+        total_booking_ctr = data_200hr["Name of student"].count()
+
+        # Display Total Booking in a similar format as provided in the image
+        st.markdown("""
+        <div style='display: flex; justify-content: space-around; align-items: center; font-size: 24px; font-weight: bold;'>
+            <div style='text-align: center;'>
+                Total Booking
+                <div style='color: #333333; font-size: 48px;'>{}</div>
+            </div>
+        </div>
+        """.format(total_booking_ctr), unsafe_allow_html=True)
+             
+
