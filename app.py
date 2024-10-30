2d8923763847cfb39ae769f612853311cbd2d9d2
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-10-30 08:58:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ location = st.sidebar.selectbox("Choose a Location:", ["Bali", "India"])
 st.image("https://raw.githubusercontent.com/antoniusawe/student_database/main/images/house%20of%20om.png",  
          use_column_width=True)
 st.markdown("<h1 style='text-align: center; font-size: 50px;'>HOUSE OF OM - DASHBOARD</h1>", unsafe_allow_html=True)
+
+if location == "India":
+    program = st.selectbox("Choose a Program:", ["200HR", "300HR"])
+else:
+    st.write("Select 'India' from the sidebar to view program options.")
