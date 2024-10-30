e97ded6c6cdd1d574f17e6b54848f003c1ed06a6
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-10-30 08:49:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.image("https://raw.githubusercontent.com/antoniusawe/student_database/main/images/house%20of%20om.png",  
          use_column_width=True)
-st.write("# HOUSE OF OM - DASHBOARD")
+st.markdown("<h3 style='text-align: center;'>HOUSE OF OM - DASHBOARD</h3>", unsafe_allow_html=True)
