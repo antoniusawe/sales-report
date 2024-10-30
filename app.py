23103e1fb70a4651523d59740b82a66bc53e2b87
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-10-30 09:40:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ if location == "India":
                     <div style='font-size: 48px; font-weight: bold;'>{total_booking_ctr}</div>
                     <div style='color: red; font-size: 14px; font-weight: bold;'>Number of Students</div>
                 </div>
-                <div style='text-align: left;'>
+                <div style='text-align: center;'>
                     <div style='font-size: 16px; font-weight: bold; color: #333333;'>Total Payable</div>
                     <div style='font-size: 48px; font-weight: bold;'>${total_payable_sum:,.2f}</div>
                     <div style='color: red; font-size: 14px; font-weight: bold;'>Total in USD</div>
