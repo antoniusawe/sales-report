5241ea7aaa4b98863bc0e4af4a071f53c458846d
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-10-30 08:53:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+st.sidebar.title("Sidebar Title")
+st.sidebar.write("This is a sidebar. Add your elements here.")
+
 st.image("https://raw.githubusercontent.com/antoniusawe/student_database/main/images/house%20of%20om.png",  
          use_column_width=True)
 st.markdown("<h1 style='text-align: center; font-size: 50px;'>HOUSE OF OM - DASHBOARD</h1>", unsafe_allow_html=True)
