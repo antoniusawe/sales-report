230f26962a64a674a96f4902a99dd626d645faea
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-10-30 09:55:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ if location == "India":
                     <div style='font-size: 48px; '>{total_payable_sum:,.0f}</div>
                     <div style='color: #202fb2; font-size: 18px; '>in USD or USD equiv</div>
                 </div>
-                <div style='text-align: center;'>
+                <div style='text-align: left;'>
                     <div style='font-size: 16px; color: #333333;'>Outstanding</div>
                     <div style='font-size: 48px;'>{outstanding_sum:,.0f}</div>
                     <div style='color: #202fb2; font-size: 18px;'>in USD or USD equiv</div>
