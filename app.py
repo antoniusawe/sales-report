2e5e8967aef2ec874e8d76dab307b2f147c68aa3
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-10-30 08:46:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
-st.image("https://raw.githubusercontent.com/antoniusawe/student_database/main/images/house%20of%20om.png", use_column_width=True)
+st.image("https://raw.githubusercontent.com/antoniusawe/student_database/main/images/house%20of%20om.png", 
+         caption="House Of Om - Sales Report", 
+         use_column_width=True)
