8eb57e5fd5eec7aa67829400a7f7fc88a15f8f8c
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-10-30 08:54:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.sidebar.title("Sidebar Title")
-st.sidebar.write("This is a sidebar. Add your elements here.")
+# st.sidebar.title("Sidebar Title")
+st.sidebar.write("Choose location")
 
 st.image("https://raw.githubusercontent.com/antoniusawe/student_database/main/images/house%20of%20om.png",  
          use_column_width=True)
