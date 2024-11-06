43115475b7a43ce1a8be9477febc5db198d826ea
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-11-06 11:21:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -350,7 +350,7 @@ if location == "Bali":
 
             with rows[row_index][col_index]:
                 st.markdown(f"""
-                    <div style='text-align: center; width: 200px; min-width: 200px; max-width: 200px; border: 1px solid #ddd; padding: 20px; margin: 10px;'>
+                    <div style='text-align: center; width: 200px; padding: 20px; margin: 10px;'>
                         <div style='font-size: 16px; color: #333333;'><strong>Site:</strong> {site_name}</div>
                         <br>
                         <div style='font-size: 48px; color: #202fb2;'>{total_available}</div>
