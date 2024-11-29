060bcde3ee27c0160c26392f7be868ccc0a4e94c
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-11-29 17:13:42 +0800

    Update ytt.py

--- pages/ytt.py
+++ pages/ytt.py
@@ -6,7 +6,7 @@ import pages.location as location
 import pages.batch as batch
 import pages.occupancy as occupancy
 
-def show(df_sales, df_occupancy):
+def show(df_sales, df_occupancy, df_leads):
     # Tambahkan opsi "All" untuk produk
     product_options = ["All", "200HR", "300HR"]  # Tambahkan opsi secara manual
     product_choice = st.radio("Choose product:", product_options, horizontal=True, key="product_choice")
@@ -33,6 +33,12 @@ def show(df_sales, df_occupancy):
                 st.error("Kolom 'BATCH START DATE' tidak ditemukan dalam occupancy data.")
                 return
 
+        if 'CREATED_AT' in df_leads.columns:
+            df_leads['CREATED_AT'] = pd.to_datetime(df_leads['CREATED_AT'], errors='coerce')
+            df_leads['WEEK'] = df_leads['CREATED_AT'].apply(lambda x: f"WEEK {((x.day - 1) // 7) + 1}" if pd.notnull(x) else None)
+            df_leads['YEAR'] = df_leads['CREATED_AT'].dt.year.astype(int)
+            df_leads['MONTH'] = df_leads['CREATED_AT'].dt.month_name().str.upper()
+
         # Dropdown untuk memilih Year, hilangkan .0
         years = ["All"] + sorted(filtered_df_sales['YEAR'].dropna().astype(int).unique().tolist())
         selected_year = st.selectbox("Select Year", years, key="year_select")
@@ -40,6 +46,9 @@ def show(df_sales, df_occupancy):
         # Filter data berdasarkan Year (kecuali jika "All" dipilih)
         if selected_year != "All":
             filtered_df_sales = filtered_df_sales[filtered_df_sales['YEAR'] == selected_year]
+            filtered_df_leads = df_leads[df_leads['YEAR'] == int(selected_year)]
+        else:
+            filtered_df_leads = df_leads.copy()
 
             # Konversi kolom Month menjadi string nama bulan (jika berubah menjadi datetime)
             filtered_df_sales['MONTH'] = pd.to_datetime(filtered_df_sales['MONTH'], format='%B', errors='coerce').dt.strftime('%B')
@@ -51,6 +60,7 @@ def show(df_sales, df_occupancy):
             # Filter data berdasarkan Month yang dipilih
             if selected_month != "All":
                 filtered_df_sales = filtered_df_sales[filtered_df_sales['MONTH'] == selected_month]
+                filtered_df_leads = filtered_df_leads[filtered_df_leads['MONTH'] == selected_month]
 
             # Membuat dropdown untuk memilih WEEK
             weeks = ["All"] + filtered_df_sales['WEEK'].dropna().unique().tolist()
@@ -59,6 +69,7 @@ def show(df_sales, df_occupancy):
             # Filter data berdasarkan WEEK yang dipilih
             if selected_week != "All":
                 filtered_df_sales = filtered_df_sales[filtered_df_sales['WEEK'] == selected_week]
+                filtered_df_leads = filtered_df_leads[filtered_df_leads['WEEK'] == selected_week]
         else:
             selected_month = "All"  # Jika "All" dipilih di Year, otomatis bulan menjadi "All"
             selected_week = "All"  # Jika "All" dipilih di Year, otomatis minggu menjadi "All"
@@ -76,7 +87,7 @@ def show(df_sales, df_occupancy):
 
     # Menampilkan konten berdasarkan pilihan tampilan
     if view_choice == "Booking":
-        booking.show_booking(filtered_df_sales)
+        booking.show_booking(filtered_df_sales, filtered_df_leads)
     elif view_choice == "Location":
         location.show_location(filtered_df_sales)
     elif view_choice == "Batch":
